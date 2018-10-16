385fc16f7ba5236691faaf6e330554525785885d
jensdufourhogent <jens.dufour.y2321@student.hogent.be>
2018-10-16 17:19:48 +0200

    Powershell DC1 en DC2

---
+++ scripts/DC1.ps1
@@ -0,0 +1,19 @@
+#Declaring variables
+$hostname = "ns1"
+$domainname = "jenduf.gent"
+
+#Computer naam wijzigen
+#Rename-Computer -ComputerName $env:COMPUTERNAME  -newName $hostname -Force -Restart
+
+#IP-adres en default gateway wijzigen
+New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress "192.168.1.1" -PrefixLength 24
+
+#Firewall uitschakelen
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+#DC en DNS installeren en domeinnaam instellen
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest -DomainName $domainname -SafeModeAdministratorPassword:(ConvertTo-SecureString -String "Project2018" -AsPlainText -Force) -Force
+
+#Server opnieuw opstarten
+Restart-computer
