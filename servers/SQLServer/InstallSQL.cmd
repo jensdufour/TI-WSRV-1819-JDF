fd61538a84c1100834c2cebc97a20d6e3289010d
jensdufourhogent <jens.dufour.y2321@student.hogent.be>
2018-10-23 14:59:24 +0200

    RM

--- servers/SQLServer/InstallSQL.cmd
+++
@@ -1,9 +0,0 @@
-xcopy /S C:\Users\Administrator\Documents\Scripts E:\Setup\Scripts\
-xcopy /S C:\Users\Administrator\Documents\Installattie E:\Setup\Bestanden\
-
-powershell "'C:\Users\Administrator\Documents\Scripts\SQL_1.ps1'"
-powershell "'C:\Users\Administrator\Documents\Scripts\SQL_2.ps1'"
-powershell "'C:\Users\Administrator\Documents\Scripts\SQL_3.ps1'"
-powershell "'C:\Users\Administrator\Documents\Scripts\SQL_4.ps1'"
-powershell "'C:\Users\Administrator\Documents\Scripts\SQL_5.ps1'"
-powershell "'C:\Users\Administrator\Documents\Scripts\SQL_6.ps1'"
