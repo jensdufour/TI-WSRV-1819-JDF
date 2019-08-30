ada714193bc094f39d4915d261c0b3c8673fc689
Jens Du Four <jens.dufour@outlook.com>
2019-08-30 11:14:01 +0200

    Exchange (DL NEEDED)

--- scripts/Exchange.ps1
+++ scripts/Exchange.ps1
@@ -31,16 +31,17 @@ function joinDomain {
 # Installing prerequisites for Exchange Server
 # -------------------------------------------------------------------------
 function changePrerquisites {
-    $chromeMSIDownloadLink
-    $UCMADownloadLink
-    $DOTNETDownloadLink
-    $VCRedistDownloadLink
+    $chromeEnterpriseDownloadLink = https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BAB3972F4-91EE-53EB-4009-AFA933E367B2%7D%26lang%3Den%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dtrue%26ap%3Dx64-stable-statsdef_0%26brand%3DGCEB/dl/chrome/install/GoogleChromeEnterpriseBundle64.zip
+    $UCMADownloadLink = https://download.microsoft.com/download/3/9/B/39B042C0-8ACB-4D1A-BE02-4E20247A36E6/UcmaSdkSetup.exe
+    $DOTNETDownloadLink = https://download.visualstudio.microsoft.com/download/pr/c76aa823-bbc7-4b21-9e29-ab24ceb14b2d/9de2e14be600ef7d5067c09ab8af5063/dotnet-sdk-2.2.401-win-x64.exe
+    $VCRedistDownloadLink = https://aka.ms/vs/16/release/vc_redist.x64.exe
     Set-Location C:/
-    Invoke-WebRequest $chromeMSIDownloadLink -OutFile chromeMSI.msi
+    Invoke-WebRequest $chromeEnterpriseDownloadLink -OutFile GoogleChromeEnterpriseBundle.zip
+    Expand-Archive GoogleChromeEnterpriseBundle.zip
     Invoke-WebRequest $UCMADownloadLink -OutFile UCMA.exe
     Invoke-WebRequest $DOTNETDownloadLink -OutFile DOTNET.exe
     Invoke-WebRequest $VCRedistDownloadLink -OutFile VCRedist.exe
-    .\chromeMSI.msi /q
+    .\GoogleChromeEnterpriseBundle64.zip\Installers\GoogleChromeStandaloneEnterprise64.msi /q
     .\UCMA.exe -q
     .\DOTNET.exe /passive /norestart
     .\VCRedist.exe /passive /norestart
@@ -51,11 +52,17 @@ function changePrerquisites {
 # Installing Exchange Server
 # -------------------------------------------------------------------------
 function changeExchange { 
-    $exchangeISO
+    $domainName = "AXXESTRAINEE"
+    #Add the downloadlink for the Exchange Server iso here!
+    $exchangeDownloadLink
+    Set-Location C:/
+    Invoke-WebRequest $exchangeDownloadLink -OutFile Exchange.iso
+    Mount-DiskImage -ImagePath .\Exchange.iso
+    $exchangeISO = (Get-DiskImage ".\Exchange.iso" | Get-Volume).DriveLetter
     Install-WindowsFeature RSAT-ADDS
     Set-Location $exchangeISO
     .\Setup.exe /PrepareSchema /IAcceptExchangeServerLicenseTerms 
-    .\Setup.exe /PrepareAD /OrganizationName:"RED" /IAcceptExchangeServerLicenseTerms
+    .\Setup.exe /PrepareAD /OrganizationName:$domainName /IAcceptExchangeServerLicenseTerms
     #Install prerequisites
     Install-WindowsFeature NET-Framework-45-Features, RPC-over-HTTP-proxy, RSAT-Clustering, RSAT-Clustering-CmdInterface, RSAT-Clustering-Mgmt, RSAT-ADDS, RSAT-Clustering-PowerShell, Web-Mgmt-Console, WAS-Process-Model, Web-Asp-Net45, Web-Basic-Auth, Web-Client-Auth, Web-Digest-Auth, Web-Dir-Browsing, Web-Dyn-Compression, Web-Http-Errors, Web-Http-Logging, Web-Http-Redirect, Web-Http-Tracing, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Lgcy-Mgmt-Console, Web-Metabase, Web-Mgmt-Console, Web-Mgmt-Service, Web-Net-Ext45, Web-Request-Monitor, Web-Server, Web-Stat-Compression, Web-Static-Content, Web-Windows-Auth, Web-WMI, Windows-Identity-Foundation
     .\Setup.exe /M:Install /R:Mailbox, ManagementTools /IAcceptExchangeServerLicenseTerms
